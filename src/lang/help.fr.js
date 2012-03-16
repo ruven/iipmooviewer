ff374e1a352b7117728a7f4919f2bcfc92791cb8
Ruven <gruvster@gmail.com>
2012-03-16 23:16:24 +0100

    Minor changes to English localization. Added French localization.

---
+++ src/lang/help.fr.js
@@ -0,0 +1,16 @@
+IIPMooViewer.lang = {
+	'help': "Aide",
+	'scale': "Echelle mobile",
+	'navigate': "Pour naviguer dans l'image: faites glisser l'image dans la fenêtre principale, ou déplacez la zone de sélection dans la fenêtre de navigation, ou cliquez sur une zone dans la fenêtre de navigation",
+	'zoomIn': "Pour agrandir l'image: double-cliquez, ou utilisez la molette de la souris, ou simplement appuyez sur la touche «+»",
+	'zoomOut': "Pour réduire l'image: maintenez enfoncée la touche «shift» et double-cliquez, ou utilisez la molette de la souris, ou simplement appuyez sur la touche «-»",
+	'rotate': "Pour faire pivoter l'image dans le sens horaire: appuyez sur la touche «r», pour le sens antihoraire; maintenez enfoncée la touche «shift» et appuyez sur la touche «r»",
+	'fullscreen': "Pour activer le mode plein écran: appuyez sur la touche «f»",
+	'annotations': "Pour masquer/afficher les annotations: appuyez sur la touche «a»",
+	'navigation': "Pour masquer/afficher la fenêtre de navigation : appuyez sur la touche «h»",
+	'more': "Pour plus d'informations, consultez le site de IIPImage",
+	'exitFullscreen': "Pour quitter le plein écran: appuyez sur la touche «Esc»",
+	'loading': "chargement",
+	'drag': "* Faites glisser pour déplacer la fenetre de navigation<br/>* Double-cliquez pour masquer/afficher les bouttons<br/>* Appuyez sur la touche «h» pour masquer la fenêtre de navigation",
+	'annotationsDisabled': "Annotations désactivés<br/>Appuyez sur la touch «a» pour les réactiver"
+};
