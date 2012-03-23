bd712584a9d373cb432ff59cda812c790fc7bdff
Ruven <gruvster@gmail.com>
2012-03-23 17:49:01 +0100

    Added Dutch localization. Translation by Edu Hackenitz of University Library Utrecht.

---
+++ src/lang/help.nl.js
@@ -0,0 +1,16 @@
+IIPMooViewer.lang = {
+	'help': 'Klik voor help',
+	'scale': 'Versleepbare schaal',
+	'navigate': 'Navigeren binnen de afbeelding: sleep de afbeelding binnen het hoofdvenster of sleep het gemarkeerde gebied binnen het navigatievenster of klik op een gedeelte binnen dit navigatievenster',
+	'zoomIn': 'Dubbelklik met de muis of gebruik het muiswieltje (scrollen); u kunt ook de "+" toets op uw toetsenbord gebruiken.',
+	'zoomOut': 'Shift toets ingedrukt houden en dubbelklikken met de muis of gebruik het muiswieltje (scrollen); u kunt ook "-" toets gebruiken',
+	'rotate': 'Roteren met de klok mee: toets "r", tegen de klok in: druk op shift en "r"',
+	'fullscreen': 'Volledig scherm: toets "f"',
+	'annotations': 'Tonen/verbergen annotaties: toets "a"',
+	'navigation': 'Tonen/verbergen navigatievenster: toets "h"',
+	'more': 'Voor meer informatie bezoek',
+	'exitFullscreen': 'Toets "Esc" om de volledige schermweergave te verlaten.',
+	'loading': 'Laden',
+	'drag': '* Sleep om te verplaatsen<br/>* Dubbel klik om de knoppen te verbergen of te tonen<br/>* Toets "h" om te verbergen',
+	'annotationsDisabled': 'Annotaties uitgeschakeld<br/>Toets "a" om deze weer in te schakelen'
+};
