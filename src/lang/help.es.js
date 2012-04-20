8b98dc56a79e82e70771ba903d518c226ead385b
Ruven <gruvster@gmail.com>
2012-04-20 16:33:11 +0200

    New Spanish localization. Thanks to David García San León.

---
+++ src/lang/help.es.js
@@ -0,0 +1,16 @@
+IIPMooViewer.lang = {
+	'help': '"click" para ayuda',
+	'scale': 'escala móvil',
+	'navigate': 'Para navegar por la imagen: arrastrar imagen dentro de la ventana principal, o arrastrar zona dentro de la ventana de navegación, o hacer "click" en un área dentro de ella',
+	'zoomIn': 'Para acercar: doble "click", o usar la rueda del ratón, o simplemente pulsar la tecla "+"',
+	'zoomOut': 'Para alejar: tecla mayúsculas y doble "click", o usar la rueda del ratón, o pulsar la tecla "-"',
+	'rotate': 'Para rotar la imagen en sentido horario: presionar la tecla "r", anti-horario: presionar mayúsculas y "r"',
+	'fullscreen': 'Para pantalla completa: presionar la tecla "f"',
+	'annotations': 'Para ocultar anotaciones: presionar la tecla "a"',
+	'navigation': 'Para mostrar/ocultar ventana de navegación: presionar la tecla "h"',
+	'more': 'Para más información visitar',
+	'exitFullscreen': 'Presionar "Esc" para salir del modo pantalla completa',
+	'loading': 'cargando',
+	'drag': '* Arrastrar para mover<br/>* Doble "click" para mostrar/ocultar botones<br/>* Presionar "h" para ocultar',
+	'annotationsDisabled': 'Anotaciones desactivadas<br/>Presionar "a" para reactivar'
+};
