04466c44c1a4648099e53c3f9edc2e2f89d48074
ruven <gruvster@gmail.com>
2012-09-21 22:41:18 +0200

    New Estonian localization. Thanks to the National Archives of Estonia.

---
+++ src/lang/help.et.js
@@ -0,0 +1,16 @@
+IIPMooViewer.lang = {
+	help: 'Kasutusjuhend',
+	scale: 'Skaala, mida saab hiirega lohistada',
+	navigate: 'Liigutamiseks: lohista pilti hiirega, lohista kasti navigatsiooniaknas või kasuta nooleklahve',
+	zoomIn: 'Suurendamiseks: tee topeltklõps, kasuta hiire kerimisnuppu või vajuta "+" klahvi',
+	zoomOut:'tee topeltklõps hoides all "shift" klahvi, kasuta hiire kerimisnuppu või vajuta "-" klahvi',
+	rotate: 'Pildi pööramiseks kellaosuti suunas vajuta "r", vastupidises suunas pööramiseks vajuta korraga "shift" ja "r"',
+	fullscreen: 'Täisekraanireziimi käivitamiseks vajuta "f" klahvi',
+	annotations: 'Annotatsioonide sisse- ja väljalülitamiseks vajuta "a" klahvi',
+	navigation: 'Navigatsiooniakna sisse- ja väljalülitamiseks vajuta "h" klahvi',
+	more: 'Rohkem infot',
+	exitFullscreen: 'Täisekraanireziimist väljumiseks vajuta "Esc" klahvi',
+	loading: 'laadib',
+	drag: '* Liigutamiseks lohista hiirega<br/>* Nuppude näitamiseks/varjamiseks tee topeltklõps<br/>* Navigatsiooniakna varjamiseks tervikuna vajuta "h" klahvi',
+	annotationsDisabled: 'Annotatsioonid on välja lülitatud<br/>Sisselülitamiseks vajuta "a" klahvi'
+}; 
