638a910649e806c85bf41e563181754620cc154b
ruven <gruvster@gmail.com>
2012-09-28 16:39:42 +0200

    New Turkish localization. Thanks to Serkan Kesim.

---
+++ src/lang/help.tr.js
@@ -0,0 +1,16 @@
+﻿IIPMooViewer.lang = {
+	'help': 'Yardım için tıklayınız',
+	'scale': 'Ölçek',
+	'navigate': 'Resmi gezinmek için: resmi ana pencere içinde sürükleyiniz veya gezinti bölmesi içinde bir alana tıklayınız',
+	'zoomIn': 'Yakınlaştırmak için: çift tıklayınız veya fare kaydırma tekerleğini kullanınız veya "+" tuşuna basınız',
+	'zoomOut': 'Uzaklaştırmak için: üstkrkt+çift tıklayınız veya fare kaydırma tekerleğini kullanınız veya "-" tuşuna basınız',
+	'rotate': 'Resmi saat yönünde döndürmek için: "r" tuşuna basınız, saat yönünün tersine döndürmek için: üstkrkt + "r" tuşuna basınız',
+	'fullscreen': 'Tam ekran modu için: "f" tuşuna basınız',
+	'annotations': 'Notlar arasında geçiş yapmak için: "a" tuşuna basınız',
+	'navigation': 'Gezinti bölmesini açmak veya kapatmak için: "h" tuşuna basınız',
+	'more': 'Daha fazla bilgi için',
+	'exitFullscreen': 'Tam ekran modundan çıkmak için "Esc" tuşuna basınız',
+	'loading': 'Yükleniyor',
+	'drag': '* Taşımak için sürükleyiniz<br/>* Düğmeleri göstermek veya gizlemek için çift tıklayınız<br/>* Gizlemek için "h" tuşuna basınız',
+	'annotationsDisabled': 'Notlar devre dışı<br/>Yeniden etkinleştirmek için "a" tuşuna basınız'
+};
