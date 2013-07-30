18f6478d41f17ca7e3be02af6da2cb8f5d94ff39
Ruven <ruven@free.fr>
2013-07-30 12:26:10 +0200

    New German, Greek and Portuguese localizations. Thanks to Markus Muhr, Anastasia Gasia and Nuno Freire of the European Library.

---
+++ src/lang/help.de.js
@@ -0,0 +1,16 @@
+IIPMooViewer.lang = {
+    'help': 'Klicken Sie bitte um Hilfstext anzuzeigen!',
+    'scale': 'Ziehbare Skala',
+    'navigate': 'Um innerhalb des Bildes zu navigieren: Ziehen sie den hervorgehobenen Ausschnitt im Hauptbild oder ziehen sie den hervorgehobenen Ausschnitt im Navigationsbild oder klicken Sie innerhalb des Navigationsbildes.',
+    'zoomIn': 'Um den Bildausschnitt zu vergrößern: Doppelklicken Sie oder verwenden Sie das Mausrad oder drücken Sie einfach die Taste "+"',
+    'zoomOut': 'Um den Bildausschnitt zu verkleinern: Shift-Doppelklicken Sieoder benutzen Sie das Mausrad oder drücken Sie die Taste "-"',
+    'rotate': 'Um das Bild im Uhrzeigersinn zu rotieren: Drücken Sie die Taste "R", gegen den Uhrzeigersinn: Drücken Sie SHIFT und "r"',
+    'fullscreen': 'Für Vollbild: Drücken Sie die Taste "f"',
+    'annotations': 'Um alle Anmerkungen zu zeigen: Drücken Sie die Taste "a"',
+    'navigation': 'Um das Navigationsfenster zu zeigen/verstecken: Drücken Sie die Taste "h"',
+    'more': 'Für mehr Information besuchen Sie',
+    'exitFullscreen': 'Drücken Sie "Esc", um den Vollbildmodus zu verlassen',
+    'loading': 'Laden',
+    'drag': '* Ziehen um zu bewegen<br/>* Doppelklicken zum Anzeigen / Ausblenden der Buttons<br/>* Drücken Sie h um sie zu verstecken',
+    'annotationsDisabled': 'Anmerkungen sind deaktiviert<br/>Drücken Sie "a", um Anmerkungen wieder zu aktivieren'
+};
---
+++ src/lang/help.pt.js
@@ -0,0 +1,16 @@
+IIPMooViewer.lang = {
+	 'help': 'prima para ajuda',
+	 'scale': 'escala ajustável',
+	 'navigate': 'Para navegar numa imagem: arraste a imagem dentro da janela principal ou arraste a zona dentro da janela de navegação ou clique numa área dentro da janela de navegação',
+	 'zoomIn': 'Para aumentar o zoom: duplo clique ou use a roda de scroll do rato ou simplesmente prima a tecla "+"',
+	 'zoomOut': 'Para reduzir o zoom: prima "shift" e duplo clique ou use a roda de scroll do rato ou simplesmente prima a tecla "-"',
+	 'rotate': 'Para rodar a imagem no sentido dos ponteiros do relógio: prima a tecla "r", para rodar no sentido inverso: prima "shift" e "r"',
+	 'fullscreen': 'Para ver no ecran completo: prima a tecla "f"',
+	 'annotations': 'Para ativar/desativar anotações: prima a tecla "a"',
+	 'navigation': 'Para ver/esconder a janela de navegação: prima a tecla "h"',
+	 'more': 'Para mais informação visite',
+	 'exitFullscreen': 'Prima "Esc" para sair do modo de ecran completo',
+	 'loading': 'a carregar',
+	 'drag': '* Arraste para mover<br/>* Duplo Clique para ver/esconder botões<br/>* Prima "h" para esconder',
+	 'annotationsDisabled': 'Anotações desativadas<br/>Prima "a" para reativar'
+};
