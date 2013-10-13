1635590f31a27fbcbdc8da58bea13154f9917d2d
Ruven <ruven@free.fr>
2013-10-13 15:43:19 +0200

    Added IIIF protocol support - thanks to implementation from Yale Center for British Art

---
+++ src/protocols/iiif.js
@@ -0,0 +1,66 @@
+/* IIIF Protocol Handler
+ */
+
+Protocols.IIIF = new Class({
+
+  /* Return metadata URL
+   */
+  getMetaDataURL: function(server,image){
+    return server + "/" + image + "/info.json";
+  },
+
+  /* Return an individual tile request URL
+   */
+  getTileURL: function(server,image,resolution,sds,contrast,k,x,y){
+    var f = this.getMultiplier(resolution);
+    var orig_x = x*f;
+    var orig_y = y*f;
+    var tile_x = this.tileSize.w * Math.pow(2,  this.num_resolutions - resolution - 1);
+    var tile_y = this.tileSize.h * Math.pow(2,  this.num_resolutions - resolution - 1);
+    var src = server + "/" + image + "/" + orig_x + "," + orig_y + "," + tile_x + "," + tile_y;
+    if (tile_x != 256) {
+      src += "/!256,256";
+    } else {
+      src += "/full";
+    }
+    src += "/0/native.jpg";
+    return src;
+  },
+
+  /* Parse an IIIF protocol metadata request
+   */
+  parseMetaData: function(response){
+    var p = eval("(" + response + ")");
+    var w = parseInt(p.width);
+    var h = parseInt(p.height);
+    this.num_resolutions = p.scale_factors.length;
+    this.tileSize = { w: 256, h: 256 };
+    var result = {
+      'max_size': { w: w, h: h },
+      'tileSize': this.tileSize,
+      'num_resolutions': this.num_resolutions
+    };
+    return result;
+  },
+
+  /* Return URL for a full view
+   */
+  getRegionURL: function(server,image,region,width){
+    return server + "/" + image + "/" + region.x + "," + region.y
+      + "," + region.w + "," + region.h + "/" + width + ",/0/native.jpg";
+  },
+
+  /* Return thumbnail URL
+   */
+  getThumbnailURL: function(server,image,width){
+    return server + "/" + image + "/full/" + width + ",/0/native.jpg";
+  },
+
+  /* Like Djatoka, IIIF wants the region offests in terms of the highest resolution it has.
+   * Here, we multiply up the offsets to that resolution.
+   */
+  getMultiplier: function(resolution) {
+    return this.tileSize.w * Math.pow(2, this.num_resolutions - resolution - 1);
+  }
+
+});
