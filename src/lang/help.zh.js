4f7d7f7a32b2fabc5b0c5714677db60dec645215
Ruven <ruven@free.fr>
2015-06-15 15:17:18 +0200

    New Chinese localization - many thanks to Bangyong Sun.

---
+++ src/lang/help.zh.js
@@ -0,0 +1,16 @@
+IIPMooViewer.lang = {
+	'help': '点击帮助',
+	'scale': '拖拽缩放',
+	'navigate': '图像导航器：在导航器窗口内拖拽图像或者点击图像以调整主窗口中的图像位置',
+	'zoomIn': "放大图像: 双击或者鼠标滚轮或者按‘+’键对图像进行放大",
+	'zoomOut': "缩小: 按shift并双击，或鼠标滚轮，或者按‘-’键进行图像缩小",
+	'rotate': "顺时针旋转图像，按‘r’键；逆时针旋转按shift加‘r’键",
+	'fullscreen': "全屏请按‘f’",
+	'annotations': "查找注释请按‘a’快捷键",
+	'navigation': "需要显示/隐藏导航器请按‘h’键",
+	'more': '需要了解更多请点击下面',
+	'exitFullscreen': "按‘Esc’跳出全屏模式",
+	'loading': '导入信息',
+	'drag': '* 拖拽移动<br/>* 双击显示/隐藏按钮<br/>* 点击h隐藏',
+	'annotationsDisabled': "隐藏注释<br/>点击‘a’恢复注释"
+};
